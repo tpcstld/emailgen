13e70f4f6b6d149b49c69a2e90d6084a5c328b95
Jerry Jiang <jerryjiang1128@gmail.com>
2015-02-20 02:38:47 -0800

    Formatting changes

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ var MAX_ADDRESSES = 1000000;
 var MAX_LENGTH = 1000;
 
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-app.get("/generate", function( request, response ) {
-    var urlParts = url.parse( request.url, true );
+app.get("/generate", function(request, response) {
+    var urlParts = url.parse(request.url, true);
     var queryParams = urlParts.query;
 
-    var domain = queryParams["domain"];
-    var numberAddresses = Math.min( queryParams["count"], MAX_ADDRESSES );
-    var seperator = queryParams["sep"];
-    var length = Math.min( queryParams["length"], MAX_LENGTH );
+    var domain = queryParams.domain;
+    var numberAddresses = Math.min(queryParams.count, MAX_ADDRESSES);
+    var seperator = queryParams.sep;
+    var length = Math.min(queryParams.length, MAX_LENGTH);
     response.setHeader('Content-Type', 'text/plain');
-    response.send( generate( domain, numberAddresses, seperator, length ) );
+    response.send(generate(domain, numberAddresses, seperator, length));
 });
 
 
-app.get('/*', function( request, response) {
+app.get('/*', function(request, response) {
     response.status(404).send("404 Page Not Found");
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
